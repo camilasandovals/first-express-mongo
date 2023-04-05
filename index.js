54ad6061cc43e179afee0c3d116d145fb1d03d34
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-05 13:52:26 -0400

    added delete

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
-import { getAllPlants, addPlant } from "./src/plants.js";
+import { getAllPlants, addPlant, deletePlant } from "./src/plants.js";
 
 const PORT = 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/plants", getAllPlants);
 app.post("/plants", addPlant);
+app.delete("/plants/:docIdentifier", deletePlant)
 //app.patch
 
 app.listen(PORT, () => {
--- src/plants.js
+++ src/plants.js
@@ -1,14 +1,25 @@
+import { ObjectId } from 'mongodb';
 import { db } from './mongoConnect.js'
 const coll = db.collection('plants')
 
 
+
+// GET
 export async function getAllPlants(req, res) {
     const plants = await coll.find({}).toArray();
     res.send(plants);
 }
 
+// POST
 export async function addPlant(req, res) { 
     const newPlant = req.body
     await coll.insertOne(newPlant);
     res.status(201).send({ message: "new plant added"})
+}
+
+// DELETE
+export async function deletePlant(req, res) {
+    const docId = { "_id": new ObjectId(req.params.docIdentifier) };
+    await coll.deleteOne( docId );
+    res.status(201).send({ message: "plant has been deleted"})
 }
